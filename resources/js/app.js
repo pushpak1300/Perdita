099f75c02636ac7dc83a54cb85c2be451eb329c1
pushpak1300 <pushpak1300@gmail.com>
2021-03-13 23:50:32 +0530

    Update( @pushpak1300)

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,7 +9,7 @@ progress.init()
 createApp({
   render: () => h(app, {
     initialPage: JSON.parse(el.dataset.page),
-    resolveComponent: name => import(`./Pages/${name}`).then(m => m.default),
+    resolveComponent: name => require(`./Pages/${name}`).default,
   })
 })
 .mixin({ methods: {
