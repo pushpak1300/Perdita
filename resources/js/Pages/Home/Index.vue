f5f42c8ce42627c279fe0ef1c35c167428d61568
Ethan Palani <46998848+Ethan0507@users.noreply.github.com>
2021-03-13 23:47:23 +0530

    Refactored Home, Profile and Item page

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -1,6 +1,33 @@
 <template>
+    <div>
+    <div class="sm:hidden">
+        <label for="question-tabs" class="sr-only">Select a tab</label>
+        <select id="question-tabs" class="block w-full rounded-md border-gray-300 text-base font-medium text-gray-900 shadow-sm focus:border-rose-500 focus:ring-rose-500">
+            <option value="#/all">All</option>
+            <option value="#/lost">Lost</option>
+            <option value="#/found">Found</option>
+        </select>
+    </div>
+    <div class="hidden sm:block">
+        <nav class="relative z-0 rounded-lg shadow flex divide-x divide-gray-200" aria-label="Tabs">
+        
+            <a href="#" aria-current="page" class="text-gray-900 rounded-l-lg  group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-6 text-sm font-medium text-center hover:bg-gray-50 focus:z-10">
+                <span>All</span>
+                <span aria-hidden="true" class="bg-rose-500 absolute inset-x-0 bottom-0 h-0.5"></span>
+            </a>
+        
+            <a href="#" aria-current="false" class="text-gray-500 hover:text-gray-700   group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-6 text-sm font-medium text-center hover:bg-gray-50 focus:z-10">
+                <span>Lost</span>
+                <span aria-hidden="true" class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"></span>
+            </a>
+        
+            <a href="#" aria-current="false" class="text-gray-500 hover:text-gray-700  rounded-r-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-6 text-sm font-medium text-center hover:bg-gray-50 focus:z-10">
+                <span>Found</span>
+                <span aria-hidden="true" class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"></span>
+            </a>
+        </nav>
+    </div>
     <div class="text-gray-600 body-font">
-        <h2 class="px-4">Home</h2>
         <div class="container px-4 py-10 mx-auto">
             <div class="flex flex-wrap -m-4">
                 <div v-for="item in items" :key="item.id"  class="lg:w-1/4 md:w-1/2 p-4 w-full">
@@ -27,6 +54,7 @@
                 </div>
             </div>
         </div>
+    </div>
     </div>
 </template>
 
--- resources/js/Pages/Profile/Index.vue
+++ resources/js/Pages/Profile/Index.vue
@@ -5,9 +5,6 @@
                 <div class="py-6 px-4 sm:p-6 lg:pb-8">
                   <div>
                     <h2 class="text-lg leading-6 font-medium text-gray-900">Profile</h2>
-                    <p class="mt-1 text-sm text-gray-500">
-                      This information will be displayed publicly so be careful what you share.
-                    </p>
                   </div>
 
                   <div class="mt-6 flex flex-col lg:flex-row">
@@ -45,27 +42,13 @@
                       <div class="mt-1 lg:hidden">
                         <div class="flex items-center">
                           <div class="flex-shrink-0 inline-block rounded-full overflow-hidden h-12 w-12" aria-hidden="true">
-                            <img class="rounded-full h-full w-full" src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=80h" alt="">
-                          </div>
-                          <div class="ml-5 rounded-md shadow-sm">
-                            <div class="group relative border border-gray-300 rounded-md py-2 px-3 flex items-center justify-center hover:bg-gray-50 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-light-blue-500">
-                              <label for="user_photo" class="relative text-sm leading-4 font-medium text-gray-700 pointer-events-none">
-                                <span>Change</span>
-                                <span class="sr-only"> user photo</span>
-                              </label>
-                              <input id="user_photo" name="user_photo" type="file" class="absolute w-full h-full opacity-0 cursor-pointer border-gray-300 rounded-md">
-                            </div>
+                            <img class="rounded-full h-full w-full" :src="$page.props.auth.user.avatar" alt="">
                           </div>
                         </div>
                       </div>
 
                       <div class="hidden relative rounded-full overflow-hidden lg:block">
-                        <img class="relative rounded-full w-40 h-40" src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=320&amp;h=320&amp;q=80" alt="">
-                        <label for="user-photo" class="absolute inset-0 w-full h-full bg-black bg-opacity-75 flex items-center justify-center text-sm font-medium text-white opacity-0 hover:opacity-100 focus-within:opacity-100">
-                          <span>Change</span>
-                          <span class="sr-only"> user photo</span>
-                          <input type="file" id="user-photo" name="user-photo" class="absolute inset-0 w-full h-full opacity-0 cursor-pointer border-gray-300 rounded-md">
-                        </label>
+                        <img class="relative rounded-full w-40 h-40" :src="$page.props.auth.user.avatar" alt="">
                       </div>
                     </div>
                   </div>
